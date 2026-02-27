97ca3e4ad79d1e7593870985871aba8b51064dd7
l3ay003 <l3ay003@gmail.com>
2026-02-27 16:03:32 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  GAS_URL: "https://script.google.com/macros/s/AKfycbw74TrW_FhOtyuZXuB7P_BV4dEP--VuPAh4j1KVjGlPmJREW5R-D0vOjm0eWTQKUVDMHQ/exec",
+  LIFF_ID: "2009201666-JexzIIEn"
+};
